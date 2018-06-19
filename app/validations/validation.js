a1fcf6d15c9f63b9e146b5e3df43d79090bba6f0
akash jain <akjain@grepruby.com>
2018-06-19 20:35:37 +0530

    verification commit

--- app/validations/validation.js
+++ app/validations/validation.js
@@ -6,17 +6,22 @@ const validateDetails = (req,res,next) => {
     console.log("inside validations");
     verificationRouter = express.Router(); 
     req.check('email', 'Invalid email address').isEmail();
-    req.check('password', 'Password is invalid').isLength({min: 1}).equals(req.body.confirmPassword);
-    req.check('mobile','enter valid mobile no').isLength({min:4});
+    req.check('password', 'Password is invalid').isLength({min:2}).equals(req.body.confirmPassword);
+    //req.check('mobile','enter valid mobile no').isLength({min:4});
 
     var errors=req.validationErrors();
     console.log("***************",typeof(errors));
+    console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&==1",req.body);
     if(errors){
         req.session.errors=errors;
         req.session.success=false;
-        res.redirect('/');
+        //req.body=body
+        console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&==2",req.body);
+        res.render('index',{success: req.session.success, errors: req.session.errors,  body:req.body});
+        console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&==3",req.body);
     }
     else{
+        console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&==4",req.body);
      next();
     }
     /*
